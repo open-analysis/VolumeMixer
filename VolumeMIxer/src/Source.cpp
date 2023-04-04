bb459ba8f32cddcbfbcd76a8cdae57f3d8c3e79e
Josh C <41541186+open-analysis@users.noreply.github.com>
2023-04-03 20:01:08 -0500

    Cleaned up
    - Removed unnecessary macros
    - Moved sendComptuerData() below functions it calls
    - Made declaration more compact

--- VolumeMIxer/src/Source.cpp
+++ VolumeMIxer/src/Source.cpp
@@ -3,49 +3,10 @@
 #include <set>
 #include "web/webClient.h"
 #include "utils/Parser.h"
+#include "utils/utils.h"
 
-#define RELEASE 1
-#define DEBUG_POST 1
-#define DEBUG_DEL 0
 #define PROG_INPUT 0
 #define DEV_INPUT 0
-
-void sendComputerData(webClient* i_client, std::vector<std::wstring>* i_names, std::set<std::wstring>* i_set, const bool i_isDevice)
-{
-	unsigned int l_count = 0;
-
-	// Check what items are in the vector vs set
-	for (auto i : *i_names)
-	{
-		l_count++;
-		// If the current item is not found in the set
-		//  Add it & send it to the server
-		if (!i_set->count(i))
-		{
-			i_set->insert(i);
-			//webPost(&i_client, i, i_isDevice);
-		}
-	}
-	// If there's more names in the set than the returned vector
-	//  then an item got deleted
-	if (l_count < i_set->size())
-	{
-		for (auto i : *i_set)
-		{
-			l_count++;
-			// If the current item is not found in the set
-			//  Add it & send it to the server
-			auto t_pos = i_set->find(i);
-			if (t_pos != i_set->end())
-			{
-				i_set->erase(t_pos);
-				//webDel(&i_client, t_pos, i_isDevice);
-			}
-		}
-	}
-
-	i_names->clear();
-}
 
 void webDel(webClient* i_client, char* i_buffer, const bool i_isDevice)
 {
@@ -86,36 +47,71 @@ void webGet(webClient* i_client, Parser* i_parser)
 	i_parser->setQueue(l_buffer);
 }
 
+void sendComputerData(webClient* i_client, std::vector<std::wstring>* i_names, std::set<std::wstring>* i_set, const bool i_isDevice)
+{
+	utils l_util = utils();
+	unsigned int l_count = 0;
+
+	// Check what items are in the vector vs set
+	for (auto i : *i_names)
+	{
+		l_count++;
+		// If the current item is not found in the set
+		//  Add it & send it to the server
+		if (!i_set->count(i))
+		{
+			i_set->insert(i);
+			webPost(i_client, (char*)l_util.convertWstr2Str(i).c_str(), i_isDevice);
+		}
+	}
+	// If there's more names in the set than the returned vector
+	//  then an item got deleted
+	if (l_count < i_set->size())
+	{
+		for (auto i : *i_set)
+		{
+			l_count++;
+			// If the current item is not found in the set
+			//  Add it & send it to the server
+			auto t_pos = i_set->find(i);
+			if (t_pos != i_set->end())
+			{
+				webDel(i_client, (char*)l_util.convertWstr2Str(*t_pos).c_str(), i_isDevice);
+				i_set->erase(t_pos);
+			}
+		}
+	}
+
+	i_names->clear();
+}
+
 void runProgram()
 {
 	webClient l_client;
 
 	AudioControl l_progOutputCntl = AudioControl();
 	l_progOutputCntl.init(eRender, eConsole);
+	std::set<std::wstring> l_devOutputNames;
 	DeviceControl l_devOutputCntl = DeviceControl();
 	l_devOutputCntl.init(eRender, eConsole);
+	std::set<std::wstring> l_progOutputNames;
 
 	Parser l_parser = Parser(&l_progOutputCntl, &l_devOutputCntl);
 
 #if PROG_INPUT
 	AudioControl l_progInputCntl = AudioControl();
 	l_progInputCntl.init(eCapture, eCommunications);
+	std::set<std::wstring> l_progInputNames;
 #endif
 #if DEV_INPUT
 	DeviceControl l_devInputCntl = DeviceControl();
 	l_devInputCntl.init(eCapture, eCommunications);
-#endif
-
-	std::set<std::wstring> l_devOutputNames;
-	std::set<std::wstring> l_progOutputNames;
-#if DEV_INPUT
 	std::set<std::wstring> l_devInputNames;
 #endif
-#if PROG_INPUT
-	std::set<std::wstring> l_progInputNames;
-#endif
+
 	std::vector<std::wstring> l_currNames;
 
+	// Main program loop
 	while (1)
 	{
 		// Get & send computer data to server
@@ -141,7 +137,6 @@ void runProgram()
 		// Get server data
 		webGet(&l_client, &l_parser);
 
-#if 0 // Current out of the program for testing. Don't want to accidentally break anything
 		// Parse received data
 		l_parser.parseQueue("out", "device");
 		l_parser.parseQueue("out", "program");
@@ -155,12 +150,12 @@ void runProgram()
 		l_parser.setAudioCntl(&l_devInputCntl);
 		l_parser.parseQueue("in", "device");
 		l_parser.setAudioCntl(&l_devOutputCntl);
-#endif
 
 #endif 
-		// Flush queue
+		// Empty the queue
 		l_parser.flushQueue();
 
+		// Wait 1 second to tax either system
 		Sleep(1000);
 	}
 
